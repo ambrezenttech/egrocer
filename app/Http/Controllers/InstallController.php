3dcf6158bdf3ada9c3f0d02ad350b76130250c4c
Showole Oladayo <show4ril@gmail.com>
2023-08-04 16:40:29 +0100

    remove purchase code

--- app/Http/Controllers/InstallController.php
+++ app/Http/Controllers/InstallController.php
@@ -86,7 +86,8 @@ class InstallController
     }
 
 
-    public function getRequirements(){
+    public function getRequirements()
+    {
 
         $phpSupportInfo = $this->requirements->checkPHPversion(
             config('installer.core.minPhpVersion')
@@ -109,7 +110,8 @@ class InstallController
     }
 
     /*Database*/
-    public function checkDatabaseConnection($database_host, $database_port, $database_name, $database_username, $database_password){
+    public function checkDatabaseConnection($database_host, $database_port, $database_name, $database_username, $database_password)
+    {
 
         $connection  = 'mysql';
 
@@ -138,23 +140,23 @@ class InstallController
             DB::connection()->getPdo();
 
             return true;
-
         } catch (\Exception $e) {
 
             return false;
         }
     }
 
-    public function setDatabase(Request $request){
+    public function setDatabase(Request $request)
+    {
 
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'database_host'     => 'required',
             'database_port'     => 'required',
             'database_name'     => 'required',
             'admin_email'       => 'required',
             'admin_password'    => 'required',
         ]);
-        if ($validator->fails   ()) {
+        if ($validator->fails()) {
             return CommonHelper::responseError($validator->errors()->first());
         }
         try {
@@ -168,7 +170,7 @@ class InstallController
             $admin_email = $request->admin_email;
             $admin_password = $request->admin_password;
 
-            if (! $this->checkDatabaseConnection($database_host, $database_port, $database_name, $database_username, $database_password) ) {
+            if (!$this->checkDatabaseConnection($database_host, $database_port, $database_name, $database_username, $database_password)) {
                 return CommonHelper::responseError("Could not connect to the database. Maybe your Database is not available.");
             }
 
@@ -195,12 +197,12 @@ class InstallController
 
                 $installedLogFile = storage_path('installed');
                 $dateStamp = date('Y/m/d h:i:sa');
-                if (! file_exists($installedLogFile)) {
-                    $message = "eGrocer Installer successfully Installed on ".$dateStamp."\n";
+                if (!file_exists($installedLogFile)) {
+                    $message = "eGrocer Installer successfully Installed on " . $dateStamp . "\n";
                     file_put_contents($installedLogFile, $message);
                 } else {
-                    $message = "eGrocer Installer successfully UPDATED on ".$dateStamp;
-                    file_put_contents($installedLogFile, $message.PHP_EOL, FILE_APPEND | LOCK_EX);
+                    $message = "eGrocer Installer successfully UPDATED on " . $dateStamp;
+                    file_put_contents($installedLogFile, $message . PHP_EOL, FILE_APPEND | LOCK_EX);
                 }
 
                 \App\Models\Admin::truncate();
@@ -214,39 +216,37 @@ class InstallController
                 $superAdmin->assignRole('Super Admin');
 
                 return CommonHelper::responseSuccess("Database");
-
             } catch (\Exception $e) {
-                Log::error("Installer -> Database Error : ",[$e]);
+                Log::error("Installer -> Database Error : ", [$e]);
                 return CommonHelper::responseError("We were able to connect to the database server (which means your username and password is okay) but not able to select the database. Please make sure it exists and that the root user has permission to use the database.");
             }
-
-        } catch (\Exception $e) {
-            return CommonHelper::responseError($e->getMessage());
-        }
-
-    }
-
-    public function checkPurchaseCode(Request $request){
-
-        $validator = Validator::make($request->all(),[
-            'purchase_code'     => 'required',
-        ]);
-        if ($validator->fails()) {
-            return CommonHelper::responseError($validator->errors()->first());
-        }
-
-        try {
-
-            $response = app(StoreSettingsApiController::class)->purchaseCode($request->purchase_code,1);
-            if($response){
-
-                return CommonHelper::responseSuccess("Valid");
-            }else{
-                return CommonHelper::responseError("Invalid code supplied!");
-            }
         } catch (\Exception $e) {
             return CommonHelper::responseError($e->getMessage());
         }
     }
 
+    public function checkPurchaseCode(Request $request)
+    {
+
+        // $validator = Validator::make($request->all(),[
+        //     'purchase_code'     => 'required',
+        // ]);
+        // if ($validator->fails()) {
+        //     return CommonHelper::responseError($validator->errors()->first());
+        // }
+        return CommonHelper::responseSuccess("Valid");
+
+        // try {
+
+        //     $response = app(StoreSettingsApiController::class)->purchaseCode($request->purchase_code,1);
+        //     if($response){
+
+        //         return CommonHelper::responseSuccess("Valid");
+        //     }else{
+        //         return CommonHelper::responseError("Invalid code supplied!");
+        //     }
+        // } catch (\Exception $e) {
+        //     return CommonHelper::responseError($e->getMessage());
+        // }
+    }
 }
