6bc54f5be3fb8979fa3de5f9af29fc949dcc0f99
Showole Oladayo <show4ril@gmail.com>
2023-08-19 16:47:52 +0100

    changes

--- app/Http/Controllers/API/Customer/BasicApiController.php
+++ app/Http/Controllers/API/Customer/BasicApiController.php
@@ -685,12 +685,8 @@ class BasicApiController extends Controller
         $seller_ids = SellerCommission::where('category_id', $request->category_id)->pluck('seller_id');
 
 
-        $sellers = Seller::whereIn('sellers.id', $seller_ids)->select('sellers.id', 'sellers.name', 'sellers.street', 'sellers.store_name', 'sellers.logo', DB::raw("ROUND(6371 * acos(cos(radians(" . $request->latitude . "))
-                                * cos(radians(sellers.latitude)) * cos(radians(sellers.longitude) - radians(" . $request->longitude . "))
-                                + sin(radians(" . $request->latitude . ")) * sin(radians(sellers.latitude))), 2) AS distance"), 'cities.max_deliverable_distance')
-            ->leftJoin("cities", "sellers.city_id", "cities.id")
+        $sellers = Seller::whereIn('sellers.id', $seller_ids)
             ->where('status', Seller::$statusActive)
-            ->havingRaw("distance < cities.max_deliverable_distance")
             ->orderBy('distance', 'asc')
             ->get();
 
