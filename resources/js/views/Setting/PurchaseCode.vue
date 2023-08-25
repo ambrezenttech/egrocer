46cd8dfd7ff1e39766a5497bb44a22b9a0e3fc98
Showole Oladayo <show4ril@gmail.com>
2023-08-25 18:57:10 +0100

    changes

--- resources/js/views/Setting/PurchaseCode.vue
+++ resources/js/views/Setting/PurchaseCode.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="page-heading">
+        <!-- <div class="page-heading">
             <div class="page-title">
                 <div class="row">
                     <div class="col-12 col-md-6 order-md-1 order-last">
@@ -52,7 +52,7 @@
                     </div>
                 </form>
             </section>
-        </div>
+        </div> -->
     </div>
 </template>
 <script>
